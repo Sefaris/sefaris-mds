180e4141acbaf72b60d01e210dfdb6490b3eb28b
Zipperiusz <Zipperiusz0@gmail.com>
2023-09-05 19:01:31 +0200

    init

---
+++ packages/renderer/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Elo {{ APP_VERSION }}
+  </div>
+</template>
+
+
+
+<script lang="ts" setup>
+  const APP_VERSION = import.meta.env.VITE_APP_VERSION;
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  max-width: 700px;
+}
+
+</style>
