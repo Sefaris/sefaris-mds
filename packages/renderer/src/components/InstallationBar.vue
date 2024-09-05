47f67a33ad5436fadb1d52f9bb97fde59a3a4863
Zipperiusz <Zipperiusz0@gmail.com>
2024-09-05 16:34:09 +0200

    add channels to communication preload -> renderer

--- packages/renderer/src/components/InstallationBar.vue
+++ packages/renderer/src/components/InstallationBar.vue
@@ -38,9 +38,10 @@ export default defineComponent({
       },
     });
 
-    // TODO: use ports instead of postmessage
     window.addEventListener('message', event => {
-      const data = event.data as ProgressStatus;
+      if (event.data.channel !== 'update-progress') return;
+      console.log(event.data);
+      const data = event.data.progress as ProgressStatus;
       stepName.value = translate(data.actionName);
       progress.value = Number((((data.step + 1) / data.maxSteps) * 100).toFixed(0));
       progressStyle.value = `${progress.value}%`;
